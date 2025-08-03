24493afd0279b55f61dd5cfe1dd85629d6bb565c
asifbuet047 <asifbuet047@gmail.com>
2025-08-04 00:34:29 +0600

    Add some color effect for charming view

--- task_02/script.js
+++ task_02/script.js
@@ -8,75 +8,92 @@ function showAllTasks() {
   // Clear previous list
   const tasks = JSON.parse(localStorage.getItem("tasks") || "[]");
   taskList.innerHTML = "";
-  console.log(tasks);
 
   // show all tasks
-  tasks.forEach((each, index) => {
-    const item = document.createElement("li");
-    item.className =
-      "list-group-item d-flex justify-content-between align-items-center";
-    item.textContent = each.title;
-    if (!each.completed) {
-      const buttonGroup = document.createElement("div");
-      const completeButton = document.createElement("button");
-      completeButton.className = "btn btn-success btn-sm";
-      completeButton.style.borderWidth = "2px";
-      completeButton.style.borderColor = "black";
-      completeButton.style.borderStyle = "solid";
-      completeButton.style.margin = "2px";
-      completeButton.textContent = "Complete this Task";
-      completeButton.addEventListener("click", () => {
-        updateTaskAsDone(index);
-      });
-      const deleteButton = document.createElement("button");
-      deleteButton.className = "btn btn-warning btn-sm";
-      deleteButton.style.borderWidth = "2px";
-      deleteButton.style.borderColor = "black";
-      deleteButton.style.borderStyle = "solid";
-      deleteButton.style.margin = "2px";
-      deleteButton.textContent = "Delete this task";
-      deleteButton.addEventListener("click", () => {
-        const newName = prompt("Delete Your task!", each.title);
-        if (newName && newName.trim() != "") {
-          deleteTask(index);
+  if (Array.isArray(tasks)) {
+    if (tasks.length > 0) {
+      tasks.forEach((each, index) => {
+        const item = document.createElement("li");
+        item.className =
+          "list-group-item d-flex justify-content-between align-items-center";
+        item.textContent = each.title;
+
+        if (index % 2 == 0) {
+          item.style.backgroundColor = "#63B4D1";
+        } else {
+          item.style.backgroundColor = "#7699D4";
+        }
+        if (!each.completed) {
+          const buttonGroup = document.createElement("div");
+          const completeButton = document.createElement("button");
+          completeButton.className = "btn btn-success btn-sm";
+          completeButton.style.borderWidth = "2px";
+          completeButton.style.borderColor = "black";
+          completeButton.style.borderStyle = "solid";
+          completeButton.style.margin = "2px";
+          completeButton.textContent = "Complete this Task";
+          completeButton.addEventListener("click", () => {
+            updateTaskAsDone(index);
+          });
+          const deleteButton = document.createElement("button");
+          deleteButton.className = "btn btn-warning btn-sm";
+          deleteButton.style.borderWidth = "2px";
+          deleteButton.style.borderColor = "black";
+          deleteButton.style.borderStyle = "solid";
+          deleteButton.style.margin = "2px";
+          deleteButton.textContent = "Delete this task";
+          deleteButton.addEventListener("click", () => {
+            const newName = prompt("Delete Your task!", each.title);
+            if (newName && newName.trim() != "") {
+              deleteTask(index);
+            }
+          });
+          buttonGroup.appendChild(completeButton);
+          buttonGroup.appendChild(deleteButton);
+          item.appendChild(buttonGroup);
+          taskList.appendChild(item);
+        } else {
+          item.style.textDecoration = "line-through";
+          const buttonGroup = document.createElement("div");
+          const completeButton = document.createElement("button");
+          completeButton.className = "btn btn-success btn-sm";
+          completeButton.style.borderWidth = "2px";
+          completeButton.style.borderColor = "black";
+          completeButton.style.borderStyle = "solid";
+          completeButton.style.margin = "2px";
+          completeButton.textContent = "Incomplete this Task";
+          completeButton.addEventListener("click", () => {
+            updateTaskAsUnDone(index);
+          });
+          const deleteButton = document.createElement("button");
+          deleteButton.className = "btn btn-warning btn-sm";
+          deleteButton.style.borderWidth = "2px";
+          deleteButton.style.borderColor = "black";
+          deleteButton.style.borderStyle = "solid";
+          deleteButton.style.margin = "2px";
+          deleteButton.textContent = "Delete this task";
+          deleteButton.addEventListener("click", () => {
+            const newName = prompt("Delete Your task!", each.title);
+            if (newName && newName.trim() != "") {
+              deleteTask(index);
+            }
+          });
+          buttonGroup.appendChild(completeButton);
+          buttonGroup.appendChild(deleteButton);
+          item.appendChild(buttonGroup);
+          taskList.appendChild(item);
         }
       });
-      buttonGroup.appendChild(completeButton);
-      buttonGroup.appendChild(deleteButton);
-      item.appendChild(buttonGroup);
-      taskList.appendChild(item);
     } else {
-      item.style.textDecoration = "line-through";
-      const buttonGroup = document.createElement("div");
-      const completeButton = document.createElement("button");
-      completeButton.className = "btn btn-success btn-sm";
-      completeButton.style.borderWidth = "2px";
-      completeButton.style.borderColor = "black";
-      completeButton.style.borderStyle = "solid";
-      completeButton.style.margin = "2px";
-      completeButton.textContent = "Incomplete this Task";
-      completeButton.addEventListener("click", () => {
-        updateTaskAsUnDone(index);
-      });
-      const deleteButton = document.createElement("button");
-      deleteButton.className = "btn btn-warning btn-sm";
-      deleteButton.style.borderWidth = "2px";
-      deleteButton.style.borderColor = "black";
-      deleteButton.style.borderStyle = "solid";
-      deleteButton.style.margin = "2px";
-      deleteButton.textContent = "Delete this task";
-      deleteButton.addEventListener("click", () => {
-        const newName = prompt("Delete Your task!", each.title);
-        if (newName && newName.trim() != "") {
-          deleteTask(index);
-        }
-      });
-      buttonGroup.appendChild(completeButton);
-      buttonGroup.appendChild(deleteButton);
-      item.appendChild(buttonGroup);
+      const item = document.createElement("li");
+      item.className =
+        "list-group-item d-flex justify-content-between align-items-center";
+      item.textContent = "No Task has been added";
+      item.style.backgroundColor = "#7699D4";
+      item.style.color = "#000000";
       taskList.appendChild(item);
     }
-  });
+  }
 }
 
 // Function to add a new task
@@ -128,3 +145,5 @@ taskForm.addEventListener("submit", function (e) {
     alert("Please give a task title");
   }
 });
+
+showAllTasks();
